7d6ae0e811596ec88e9fa6fbef79186455ae31bf
RobinH <Robin.Hawiz.6670@student.uu.se>
2025-01-26 21:33:09 +0100

    webpack: Added scriptLoading: 'blocking' to remove defer attribute from generated script-elements

--- utils/multipage.js
+++ utils/multipage.js
@@ -6,6 +6,7 @@ const generateHtmlPlugin = (pageName) => {
       title: pageName,
       filename: `index.html`,
       template: `./src/pages/index.html`,
+      scriptLoading: "blocking",
       inject: "body",
     });
   } else {
@@ -13,6 +14,7 @@ const generateHtmlPlugin = (pageName) => {
       title: pageName,
       filename: `${pageName}/index.html`,
       template: `./src/pages/${pageName}/index.html`,
+      scriptLoading: "blocking",
       inject: "body",
     });
   }
